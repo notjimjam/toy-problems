deecbeab82b87d823df4290eeb26da4445902c41
Jamie Smith <thejamiemsmith@gmail.com>
2022-04-15 10:16:08 -0700

    initial commit

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TopicBrowser extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default TopicBrowser;
---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EvenAndOdds extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <p>EvenAndOdd</p>
+      </div>
+    );
+  }
+}
